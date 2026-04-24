50d6e94ce7c342b0f3d85b54b9ff76a24840f16f
hyeaguen <hyeaguenshim@gmail.com>
2026-04-24 10:39:37 +0900

    init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,3 +11,17 @@ public class DemoApplication {
     }
 
 }
+
+public void sayHello() {
+    System.out.println("Hello from Demo1Application!");
+}
+
+public int addNumbers(int a, int b) {
+    return a + b;
+}
+
+public String getStatus() {
+    return "Application is running";
+}
+
+
